42c46c48e8826999c8bb8db907c1f51e9b43d3d8
KMC <KMC@KMC-PC>
2015-11-06 00:38:29 +0900

    sss

--- src/githubTest/test/Pet.java
+++ src/githubTest/test/Pet.java
@@ -3,7 +3,7 @@ package githubTest.test;
 public class Pet {
 
 	public static void main(String[] args) {
-	System.out.println("fudsfsdfsfckdddddddd");
+	System.out.println("fsssudsfsdfsfckdddddddd");
 
 	}
 
